0dd61f78b23c5128ca0944fffbda4a284ba7d8ef
Hudson Oliveira <hudson.oliveira@youtan.com.br>
2022-07-13 20:57:34 -0300

    initial commit

---
+++ src/main/java/com/ravelist/api/RavelistApiApplication.java
@@ -0,0 +1,13 @@
+package com.ravelist.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavelistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavelistApiApplication.class, args);
+	}
+
+}
